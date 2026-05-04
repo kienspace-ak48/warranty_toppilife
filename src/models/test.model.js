8f42fa4fdd947375a5943caa3015f0195dff09bd
KienVu <kienidkcgo1001@gmail.com>
2026-05-04 19:14:23 +0700

    edit homepage

--- src/models/test.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const testSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    description: {type: String, required: true},
-}, {timestamps: true});
-
-const Test = mongoose.model('Test', testSchema);
